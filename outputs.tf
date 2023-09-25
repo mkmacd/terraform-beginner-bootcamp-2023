24c101df850410005e3266c305f404299d7dae2d
Mike Macdonald <mike@macmacphotography.com>
2023-09-25 20:29:50 +0000

    33 Enabled static website hosting, uploaded index and error file via terraform

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "bucket_name" {
     description = "Bucket name for our static website hosting"
     value = module.terrahouse_aws.bucket_name
 }
+output "website_endpoint_s3"{
+    description = "S3 static website hosting endpoint"
+    value = module.terrahouse_aws.website_endpoint
+}
