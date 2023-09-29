492375797a2a0d69442fd7620a67feff40c41999
Mike Macdonald <mike@macmacphotography.com>
2023-09-29 16:16:14 +0000

    invalidate cloudfront stack when content version changes

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "website_endpoint_s3"{
     description = "S3 static website hosting endpoint"
     value = module.terrahouse_aws.website_endpoint
 }
+output "cloudfront_url"{
+    description = "Domain name for cloudfront distribution"
+    value = module.terrahouse_aws.cloudfront_url
+    }
