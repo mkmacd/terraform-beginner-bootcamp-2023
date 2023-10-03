9040131f5d1e2b4dc3af1ce80ed84dbfbaeaaabf
Mike Macdonald <mike@macmacphotography.com>
2023-10-03 13:44:01 +0000

    #57 Implement terraform provider block in terraform

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
-output "bucket_name" {
-    description = "Bucket name for our static website hosting"
-    value = module.terrahouse_aws.bucket_name
-}
-output "website_endpoint_s3"{
-    description = "S3 static website hosting endpoint"
-    value = module.terrahouse_aws.website_endpoint
-}
-output "cloudfront_url"{
-    description = "Domain name for cloudfront distribution"
-    value = module.terrahouse_aws.cloudfront_url
-    }
+# output "bucket_name" {
+#     description = "Bucket name for our static website hosting"
+#     value = module.terrahouse_aws.bucket_name
+# }
+# output "website_endpoint_s3"{
+#     description = "S3 static website hosting endpoint"
+#     value = module.terrahouse_aws.website_endpoint
+# }
+# output "cloudfront_url"{
+#     description = "Domain name for cloudfront distribution"
+#     value = module.terrahouse_aws.cloudfront_url
+#     }
